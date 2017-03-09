135658fdeaa1c9c672006d04723790b981f78d59
monsteronfire <monsteronfire@gmail.com>
2017-03-09 17:00:32 +0800

    Add policy for cookies

---
+++ app/policies/cookie_policy.rb
@@ -0,0 +1,20 @@
+class CookiePolicy < ApplicationPolicy
+  attr_reader :user, :cookie
+
+  def initialize(user, cookie)
+    @user = user
+    @cookie = cookie
+  end
+
+  def create?
+    user.admin?
+  end
+
+  def update?
+    user.admin?
+  end
+
+  def destroy
+    user.admin?
+  end
+end
